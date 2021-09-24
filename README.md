cb61c671e7b075797d9fe9ce461161756a75cb18
yuwei217 <yjia0142@student.monash.edu>
2021-09-24 21:25:16 +1000

    update

--- README.md
+++ README.md
@@ -1,29 +1,40 @@
 
 <!-- README.md is generated from README.Rmd. Please edit that file -->
 
-# shiny-template
+# Access to clean fuels for cooking vs GDP per capit
 
-Rename the above to something more meaningful.
-
-## YOUR NAME + student id
+## Yuwei Jiang + 32307322
 
 This is a template that contains materials to create a shiny gadget
 application for assessment 2. You will need the following packages
 installed to generate the gadget from this template:
 
 ``` r
-install.packages(c("shiny", "crosstalk", "plotly", "DT", "tidyverse", "here"))
+install.packages(c("shiny", "crosstalk", "plotly", "DT", "tidyverse", "here", "ggplot2"))
 ```
 
 ## How to run the app
 
-Describe how to run the gadget and use it here. Include any additional
-comments and list any additional packages required to run the gadget.
+In this shiny app, you can explore the relationships between the wealth
+of a country and the proportion of the population that have access to
+clean cooking fuels.  
+On the chart page, you can select one or more countries and move the
+year slider to explore the data. And you have the choice to hide data
+points on countries where there are less than 1 million in the upper
+right. The app also allow you to toggle between a linear axis and a log
+axis on the chart. On the table page, it shows all the years for each
+country ordered by from most recent year. And the table could update
+according to a change from the selection input provided by this shiny
+app.
 
 ## Session Info
 
-Add the session info here.
-
-``` r
-# use utils::sessionInfo() or sessioninfo::session_info()
-```
+setting value  
+version R version 4.0.5 (2021-03-31)  
+os Windows 10 x64  
+system x86\_64, mingw32  
+ui RStudio  
+language (EN)  
+collate Chinese (Simplified)\_China.936 ctype Chinese
+(Simplified)\_China.936 tz Australia/Sydney  
+date 2021-09-24
--- app.R
+++ app.R
@@ -2,12 +2,16 @@ library(shiny)
 library(DT)
 library(tidyverse)
 library(plotly)
+library(ggplot2)
 library(here)
+library(dplyr)
+library(crosstalk)
 
-# once you've prepared the data uncomment this line
+
 tidy_fuels <- read_csv(here("data", "cooking.csv"))
-# you might want to use highlight_key here
 
+# UI
+# -------------
 ui <- fluidPage(
   title = "Indoor Air Pollution",
   tabsetPanel(
@@ -24,9 +28,8 @@ ui <- fluidPage(
         column(
           6,
           offset = 1,
-          # also possible to use plotly here
           selectizeInput("countries", "Select Countries",
-            choices = NULL,
+            choices = tidy_fuels$country,
             multiple = TRUE
           )
         ),
@@ -50,21 +53,110 @@ ui <- fluidPage(
       )
     ),
     tabPanel("table", dataTableOutput("table"), icon = icon("table")),
-    tabPanel("about", icon = icon("question"))
+
+    tabPanel("about", icon = icon("question"), uiOutput("tab"), uiOutput("tab2"))
   )
 )
 
+# SERVER
+# -------------
 server <- function(input, output, session) {
-  # Define reactive expressions here for filtering data
 
-  # Define outputs here
+
   output$chart <- renderPlotly({
-    ggplotly(ggplot() +
-      geom_blank())
+
+    if (input$small_countries) {
+      tidy_fuels <- tidy_fuels %>%
+        filter(total_population >= 1e6)
+    }
+
+    if(!is.null(input$countries)){
+      tidy_fuels <- tidy_fuels %>%
+        filter(country %in% input$countries)
+    }
+
+    tidy_fuels$tooltip <-
+      glue::glue_data( tidy_fuels,
+                      "Country: {country}",
+                      "\nPopulation: {scales::label_number_auto()(total_population)}",
+                      "\nProportion: {scales::percent(cooking, scale = 1, accuracy = 1)}",
+                      "\nGDP per capita: {scales::dollar(gdp_per_capita)}")
+
+    if(input$linear_scale){
+      tidy_fuels1 <- tidy_fuels %>%
+        filter(year == input$year) %>%
+        highlight_key(~country) %>%
+        ggplot(aes(
+          x = gdp_per_capita,
+          y = cooking,
+          size = total_population,
+          color = continent,
+          label = country), alpha = 0.5
+        ) +
+        geom_point(aes(text = tooltip)) +
+        scale_x_continuous(labels = ~scales::dollar(., accuracy = 1)) +
+        scale_y_continuous(labels = ~scales::percent(., scale = 1)) +
+        scale_size_continuous(breaks = c(100e6, 500e6, 1e9),
+                              labels = c("100 million", "500 million", "1 billion"),limits = c(100e6,1e9)) +
+
+        labs(x = "GDP per capita",
+             y = "Access to clean fuels for cooking (% access)",
+             color = "continent",
+             size = "")
+    }
+
+    else{
+        tidy_fuels1 <- tidy_fuels %>%
+        filter(year == input$year) %>%
+        highlight_key(~country) %>%
+        ggplot(aes(
+          x = gdp_per_capita,
+          y = cooking,
+          size = total_population,
+          color = continent,
+          label = country), alpha = 0.5
+        ) +
+        geom_point(aes(text = tooltip)) +
+        scale_x_log10(breaks = c(1000, 2000, 5000, 10000, 20000,
+                                 50000, 100000),
+                      labels = ~scales::dollar(., accuracy = 1), limits = c(1000,100000)) +
+        scale_y_continuous(labels = ~scales::percent(., scale = 1)) +
+        scale_size_continuous(breaks = c(100e6, 500e6, 1e9),
+                              labels = c("100 million", "500 million", "1 billion"), limits = c(100e6,1e9)) +
+
+        labs(x = "GDP per capita",
+             y = "Access to clean fuels for cooking (% access)",
+             color = "continent",
+             size = "")
+    }
+
+    p <- ggplotly(
+        tidy_fuels1 ,
+        tooltip = "text") %>%
+        config(displaylogo = FALSE,
+        modeBarButtonsToRemove = c("zoomIn2d", "zoomOut2d", "zoom2d", "pan2d")) %>%
+        highlight( on = "plotly_hover", off = "plotly_deselect")
+
   })
 
   output$table <- renderDataTable({
-    NULL
+                    (tidy_fuels) %>%
+                   mutate(cooking = round(cooking, digits = 2),
+                   gdp_per_capita = round(gdp_per_capita, digits = 2)) %>%
+                   filter(year == input$year)
+
+
+
+  })
+
+  url <- a("Source Link", href="https://ourworldindata.org/grapher/access-to-clean-fuels-for-cooking-vs-gdp-per-capita")
+  output$tab <- renderUI({
+    tagList("URL link:", url)
+  })
+
+  url2 <- a("Creator: Yuwei Jiang", href="https://etc5523-2021.github.io/blog-yuwei217/about.html")
+  output$tab2 <- renderUI({
+    tagList("URL link:", url2)
   })
 }
 
